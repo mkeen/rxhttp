fdbd32833b007b743918b9d8ea9d932cede5b424
Mike Keen <mkeen.atl@gmail.com>
2018-07-19 00:39:10 -0400

    working, simple

---
+++ src/httprequest.ts
@@ -0,0 +1,95 @@
+import { Observable, Observer, Subject } from 'rxjs';
+import { takeUntil, filter } from 'rxjs/operators';
+
+interface HttpRequestState {
+  messages: string[];
+}
+
+interface HttpRequestHeaders {
+  "Content-Type": string;
+}
+
+interface HttpRequestOptions {
+  headers?: HttpRequestHeaders;
+  method?: string;
+  body?: string;
+  retry?: boolean;
+}
+
+interface NotNull {
+  body: any;
+}
+
+declare var ReadableStream: {
+  prototype: ReadableStream;
+  new(underlyingSink?: any, queueingStrategy?: any): any;
+};
+
+export class HttpRequest<T> {
+  private url: string;
+  private options: HttpRequestOptions;
+  private $cancel: Subject<boolean> = new Subject<boolean>();
+
+
+  constructor(url: string, options: HttpRequestOptions = {}) {
+    this.url = url;
+    this.options = options;
+  }
+
+  public cancel(): void {
+    console.log("about to cancel");
+    this.$cancel.next(true);
+  }
+
+  public retry(): Observable<T> {
+    return this.send();
+  }
+
+  public send(): Observable<T> {
+    return Observable.create((observer: Observer<T>) => {
+      fetch(this.url, Object.assign({
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }, this.options)).then((response: NotNull) => {
+        const reader = response.body.getReader();
+        return new ReadableStream({
+          start: (controller: any) => {
+            return next();
+            function next(): any {
+              return reader.read().then(({ done, value }: any) => {
+                if (done) {
+                  controller.close();
+                  observer.complete();
+                  return;
+                }
+
+                controller.enqueue(value);
+                let decodedValue: string;
+                let parsedDecodedValue: T;
+                try {
+                  decodedValue = new TextDecoder("utf-8").decode(value);
+                  try {
+                    parsedDecodedValue = JSON.parse(decodedValue);
+                    observer.next(parsedDecodedValue);
+                  } catch { }
+                } catch { }
+
+                return next();
+              })
+            }
+          },
+          cancel: () => {
+            console.log("this is donezo");
+          }
+        })
+      }).then(stream => {
+        return new Response(stream)
+      }).catch(() => {
+        console.log("error");
+      });
+    }).pipe(takeUntil(this.$cancel))
+      .pipe(filter((fragment: any) => !!fragment));
+  }
+}
+
---
+++ src/rxhttp.ts
@@ -0,0 +1,1 @@
+export { HttpRequest } from './httprequest';

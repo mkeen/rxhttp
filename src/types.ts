69f53937f93af1104aee660fe89b097bc10f099b
millnitzluan <luanmillnitz@gmail.com>
2018-09-04 00:33:08 -0300

    First implementation of doc

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,44 @@
 import { Observable } from 'rxjs';
 
+/**
+ * Custom Object type that will be sent in request header
+ */
 export type HttpRequestHeaders = {} & {
   [prop: string]: string;
 }
 
+/**
+ * HttpRequestOptions interface that will be used in custom request options.
+ */
 export interface HttpRequestOptions {
+  /**
+   * `headers` Custom Header to be sent.
+   */
   headers?: HttpRequestHeaders;
+  /**
+   * `method` request method to be used.
+   */
   method?: string;
+  /**
+   * `body` is the data to be sent as the request body.
+   */
   body?: string;
+  /**
+   * `retry` Cooming soon.
+   */
   retry?: boolean;
 }
 
+/**
+  * FetchBehavior enum that you can choose for real-time JSON streams or simple request.
+  */
 export enum FetchBehavior {
+  /**
+   * Real-time JSON streams option.
+   */
   stream = 'stream',
+  /**
+   * Simple request option.
+   */
   simple = 'simple'
 }
-

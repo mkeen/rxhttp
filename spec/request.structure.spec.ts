69e562b5949dc45ff5a6a3434b567a7ec8e31d76
Mike Keen <mkeen.atl@gmail.com>
2018-11-21 21:40:33 -0500

    remove unneeded bits

--- spec/request.structure.spec.ts
+++ spec/request.structure.spec.ts
@@ -1,12 +1,4 @@
 import { HttpRequest } from './../src/httprequest';
-import { TestScheduler } from 'rxjs/testing';
-import { throttleTime } from 'rxjs/operators';
-
-const scheduler = new TestScheduler((actual, expected) => {
-  // asserting the two objects are equal
-  // e.g. using chai.
-  expect(actual).toBe(expected);
-});
 
 describe('HttpRequest<TestDataType>', () => {
   const fakeRet = { 'id': 11, 'name': 'mike' }
